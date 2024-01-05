493ddf0d8243643a36e35b8ad6989849756b83f9
Viktor Sommar <viktor@planima.se>
2024-01-05 10:35:54 +0100

    Clean up message payload

--- bot.py
+++ bot.py
@@ -9,15 +9,19 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
-client.chat_postMessage(channel='#test-bot', text="Hello world!")
+@slack_event_adapter.on('message')
 
-""" @slack_event_adapter.on('message')
 def message(payload):
-  event = payload.get('event', []) """
+  event = payload.get('event', {})
+  channel_id = event.get('channel')
+  user_id = event.get('user')
+  text = event.get('text')
+
+  client.chat_postMessage(channel=channel_id, text=text)
 
 if __name__ == "__main__":
   app.run(debug=True)
