621cc21d686d3b2d3f086dee8b33ff5194ae9302
Viktor Sommar <viktor@planima.se>
2024-01-06 17:55:14 +0100

    Event based slack message

--- bot.py
+++ bot.py
@@ -15,6 +15,61 @@ client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
 message_counts = {}
+welcome_messages = {}
+
+class WelcomeMessage:
+  START_TEXT = {
+    'type': 'section',
+    'text': {
+      'type': 'mrkdwn',
+      'text': (
+        'Welcome to this awesome channel! \n\n'
+        '*Get started buy completing the tasks!*'
+      )
+    }
+  }
+
+  DIVIDER = {'type': 'divider'}
+
+  def __init__(self, channel, user):
+    self.channel = channel
+    self.user = user
+    self.icon_emoji = ':robot_face:'
+    self.timestamp = ''
+    self.completed = False
+
+  def get_message(self):
+    return {
+      'ts': self.timestamp,
+      'channel': self.channel,
+      'username': 'Welcome Robot!',
+      'icon_emoji': self.icon_emoji,
+      'blocks': [
+        self.START_TEXT,
+        self.DIVIDER,
+        self._get_reaction_task()
+      ]
+    }
+
+  def _get_reaction_task(self):
+    checkmark = ':white_check_mark:'
+
+    if not self.completed:
+      checkmark = ':white_large_square:'
+
+    text = f'{checkmark} *React to this message!*'
+
+    return {'type': 'section', 'text': {'type': 'mrkdwn', 'text': text}}
+
+def send_welcome_message(channel, user):
+  welcome = WelcomeMessage(channel, user)
+  message = welcome.get_message()
+  response = client.chat_postMessage(**message)
+  welcome.timestamp = response['ts']
+
+  if channel not in welcome_messages:
+    welcome_messages[channel] = {}
+  welcome_messages[channel][user] = welcome
 
 @slack_event_adapter.on('message')
 def message(payload):
@@ -23,11 +78,14 @@ def message(payload):
   user_id = event.get('user')
   text = event.get('text')
 
-  if BOT_ID != user_id:
+  if user_id != None and BOT_ID != user_id:
     if user_id in message_counts:
       message_counts[user_id] += 1
     else:
       message_counts[user_id] = 1
+
+    if text.lower() == 'start':
+      send_welcome_message(channel_id, user_id)
 
     client.chat_postMessage(channel=channel_id, text=text)
 
